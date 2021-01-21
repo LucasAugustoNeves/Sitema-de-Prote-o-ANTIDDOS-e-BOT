bd465133773077996e9c64ece2586101c50e0159
Neves <lucasagneves@gmail.com>
2021-01-21 21:28:14 -0200

    Pagina Inicial V0.01
    Criação da página inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+		include ("anti_ddos/start.php"); //write this at the top of your PHP application and all is done!!!
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/flottant.css">
+		<meta charset="UTF-8">
+		<title>Example Webpage</title>
+	</head>
+	<body>
+		<center>
+			<div class="container">
+				<h1>Exemplo de pagina web</h1>
+				<p>Esse é um pequeno sistema de proteção de sites de ataques de Bot e DDOS.</p>
+			</div>
+		</center>
+	</body>
+</html>
