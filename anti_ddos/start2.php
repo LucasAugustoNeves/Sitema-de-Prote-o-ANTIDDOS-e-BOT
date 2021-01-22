4694f17d24955f834afd76e1ad4f827ef09958e3
LucasAugustoNeves <lucasagneves@gmail.com>
2021-01-22 09:32:45 -0300

    coreção de links

--- anti_ddos/start2.php
+++ anti_ddos/start2.php
@@ -38,32 +38,101 @@ if(isset($_SESSION['standby'])){
 		$ad_num_query = ($ad_sec == $ad_sec_query) ? $ad_num_query++ : '1 ';
 		$ad_file = fopen ("{$ad_dir}/{$ad_check_file}", "w");
 
-		$ad_string = ($ad_num_query >= $ad_ddos_query) ? '<?php $ad_end_defense='.safe_print($ad_date + $ad_defense_time).'; ?>' : '<?php $ad_num_query='. safe_print($ad_num_query) .'; $ad_sec_query='. safe_print($ad_sec) .'; ?>';
+		$ad_string = ($ad_num_query >= $ad_ddos_query) ? '<?php $ad_end_defense='.safe_print($ad_date + $ad_defense_time).'; ?>'
+: '<?php $ad_num_query='. safe_print($ad_num_query) .'; $ad_sec_query='. safe_print($ad_sec) .'; ?>';
 
-		fputs ($ad_file, $ad_string);
-		fclose ($ad_file);
-	}
+fputs ($ad_file, $ad_string);
+fclose ($ad_file);
+}
 }else{
 
-		$_SESSION['standby'] = 1;
-		// Esse é o link que volta para a pagina atual
-		$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-		header("refresh:8,".$actual_link);
-	?>
-		<style type="text/css">
-			.loading {display: flex; flex-direction: column; align-items: center; } 
-			.loading__msg {font-family: Roboto; font-size: 16px; } 
-			.loading__dots {display: flex; flex-direction: row; width: 100%; justify-content: center; margin: 100px 0 30px 0; } 
-			.loading__dots__dot {background-color: #44BBA4; width: 20px; height: 20px; border-radius: 50%; margin: 0 5px; color: #587B7F; } 
-			.loading__dots__dot:nth-child(1) {animation: bounce 1s 1s infinite; } 
-			.loading__dots__dot:nth-child(2) {animation: bounce 1s 1.2s infinite; } 
-			.loading__dots__dot:nth-child(3) {animation: bounce 1s 1.4s infinite; } @keyframes bounce {0% {transform: translate(0, 0); } 50% {transform: translate(0, 15px); } 100% {transform: translate(0, 0); } }
-		</style>
+$_SESSION['standby'] = 1;
+// Esse é o link que volta para a pagina atual
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") .
+"://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+header("refresh:8,".$actual_link);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Captcha</title>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+	<meta name="author" content="Fabian Wennink © <?= date('Y') ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="assets/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<link href="assets\demo.css" rel="stylesheet" type="text/css">
+
+	<!-- Include IconCaptcha stylesheet -->
+	<link href="assets/css/icon-captcha.min.css" rel="stylesheet" type="text/css">
+
+	<style type="text/css">
+		.loading {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+
+		.loading__msg {
+			font-family: Roboto;
+			font-size: 16px;
+		}
+
+		.loading__dots {
+			display: flex;
+			flex-direction: row;
+			width: 100%;
+			justify-content: center;
+			margin: 100px 0 30px 0;
+		}
+
+		.loading__dots__dot {
+			background-color: #44BBA4;
+			width: 20px;
+			height: 20px;
+			border-radius: 50%;
+			margin: 0 5px;
+			color: #587B7F;
+		}
+
+		.loading__dots__dot:nth-child(1) {
+			animation: bounce 1s 1s infinite;
+		}
+
+		.loading__dots__dot:nth-child(2) {
+			animation: bounce 1s 1.2s infinite;
+		}
+
+		.loading__dots__dot:nth-child(3) {
+			animation: bounce 1s 1.4s infinite;
+		}
+
+		@keyframes bounce {
+			0% {
+				transform: translate(0, 0);
+			}
+
+			50% {
+				transform: translate(0, 15px);
+			}
+
+			100% {
+				transform: translate(0, 0);
+			}
+		}
+	</style>
+</head>
+
+<body>
 	<div class="loading" style="margin-top: 11%;">
 		<div class="loading__dots">
-		<div class="loading__dots__dot"></div>
-		<div class="loading__dots__dot"></div>
-		<div class="loading__dots__dot"></div>
+			<div class="loading__dots__dot"></div>
+			<div class="loading__dots__dot"></div>
+			<div class="loading__dots__dot"></div>
 		</div>
 		<div class="loading__msg">
 			<center>
@@ -71,12 +140,80 @@ if(isset($_SESSION['standby'])){
 					ANTIDDOS is checking....
 				</b>
 				<br><br>
-			Hi, don't worry, this is a simple security verfication, 
-			you will see this only one time;<br> your webpage will show up soon!
-			 </center></div>
+				Hi, don't worry, this is a simple security verfication,
+				you will see this only one time;<br> your webpage will show up soon!
+				</br>
+				<div class="section">
+
+					<!-- Apenas um formulário HTML básico, o captcha deve SEMPRE ser colocado DENTRO do <form> element -->
+					<h2>Form:</h2>
+
+					<?php
+                    if(isset($captchaMessage)) {
+                        echo '<b>Captcha Message: </b>' . $captchaMessage;
+                    }
+                ?>
+
+					<form action="" method="post">
+
+						<!-- Elemento que usamos para criar o IconCaptcha com -->
+						<div class="captcha-holder"></div>
+
+						<!-- Botão Enviar para testar seu IconCaptcha input -->
+						<input type="submit" value="Submit demo captcha" class="btn">
+					</form>
+				</div>
+			</center>
+		</div>
 	</div>
+	<link href="//fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet">
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+	<script src="assets/js/icon-captcha.min.js" type="text/javascript"></script>
+
+	<script async type="text/javascript">
+		$(window).ready(function () {
+			$('.captcha-holder').iconCaptcha({
+					theme: ['light', 'dark'],
+					fontFamily: '',
+					clickDelay: 500,
+					invalidResetDelay: 3000,
+					requestIconsDelay: 1500,
+					loadingAnimationDelay: 1500,
+					hoverDetection: true,
+					showCredits: 'show',
+					enableLoadingAnimation: true,
+					validationPath: 'src/captcha-request.php',
+					messages: {
+						header: "Selecione a imagem que não pertence à linha",
+						correct: {
+							top: "Great!",
+							bottom: "Você não parece ser um robô."
+						},
+						incorrect: {
+							top: "Oops!",
+							bottom: "Você selecionou a imagem errada."
+						}
+					}
+				})
+				.bind('init.iconCaptcha', function (e, id) {
+					console.log('Event: Captcha initialized', id);
+				}).bind('selected.iconCaptcha', function (e, id) {
+					console.log('Event: Icon selected', id);
+				}).bind('refreshed.iconCaptcha', function (e, id) {
+					console.log('Event: Captcha refreshed', id);
+				}).bind('success.iconCaptcha', function (e, id) {
+					console.log('Event: Correct input', id);
+				}).bind('error.iconCaptcha', function (e, id) {
+					console.log('Event: Wrong input', id);
+				});
+		});
+	</script>
+</body>
+
+</html>
 
 <?php exit();
-
 }
 ?>
